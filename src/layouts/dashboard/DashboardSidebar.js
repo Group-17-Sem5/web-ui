6f069ac1e5488ec0470d97f599e938005a5ea5e3
Kajanan98 <Kajanan023@gmail.com>
2021-07-20 17:19:31 +0530

    Removedevdependencies

--- src/layouts/dashboard/DashboardSidebar.js
+++ src/layouts/dashboard/DashboardSidebar.js
@@ -46,7 +46,7 @@ export default function DashboardSidebar({ isOpenSidebar, onCloseSidebar }) {
     if (isOpenSidebar) {
       onCloseSidebar();
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    
   }, [pathname]);
 
   const renderContent = (
